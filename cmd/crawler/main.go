fa2f5116342af8d7f53b31fed854b739639d0a8d
Orlov Anton <ion6431@gmail.com>
2021-05-08 16:13:22 +0300

    Implemented authentication

--- cmd/crawler/main.go
+++ cmd/crawler/main.go
@@ -1,18 +1,53 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
+	"net/http"
+	"net/url"
+	"strings"
 
 	"github.com/veotani/goblinator/pkg/config"
 )
 
-func main() {
+func getConfig() *config.GoblinatorConfig {
 	config, err := config.New()
 	if err != nil {
-		log.Print(err)
-	} else {
-		log.Print("Success!")
-		log.Printf("Client ID: %s\n", config.BlizzardClientId)
-		log.Printf("Client Secret: %s\n", config.BlizzardClientSecret)
+		log.Fatalf("Couldn't set up configuration: %s", err)
 	}
+	return config
+}
+
+func authenticate(clientId string, clientSecret string) *http.Response {
+	params := url.Values{}
+	params.Add("grant_type", `client_credentials`)
+	body := strings.NewReader(params.Encode())
+
+	req, err := http.NewRequest("POST", "https://eu.battle.net/oauth/token", body)
+	if err != nil {
+		log.Fatalf("Failed to create request: %s", err)
+	}
+	req.SetBasicAuth(clientId, clientSecret)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to get response: %s", err)
+	}
+
+	if resp.StatusCode != 200 {
+		log.Fatalf("Request wasn't successfull and returned code %d", resp.StatusCode)
+	}
+
+	return resp
+}
+
+func main() {
+	config := getConfig()
+	resp := authenticate(config.BlizzardClientId, config.BlizzardClientSecret)
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Couldn't read response: %s", err)
+	}
+	log.Println(string(respBody))
 }
